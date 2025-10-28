3cd933755facac0d2fae278c0965c8559e383b63
ChaudharyAnshul <ChaudharyAnshul@users.noreply.github.com>
2025-10-28 02:15:03 +0000

    deploy: d8f0cbe0d78f6b3a145d9aaec0e42eea90a419ba

--- src/config/interpersonal.js
+++
@@ -1,7 +0,0 @@
-export const InterPersonalData = {
-  categories: ['Collaborative Communication', 'Adaptability', 'Systems Thinking', 'Cross-Functional Collaboration', 'Debugging & Optimization', 'Time Management'],
-  series: [
-    { name: 'Anshul Chaudhary', data: [91, 89, 92, 91, 87, 90] },
-    { name: 'Average Human', data: [80, 75, 75, 80, 75, 70] },
-  ],
-};
--- src/config/project-diversity.js
+++
@@ -1,6 +0,0 @@
-export const projectDiversity = [
-  { label: 'Software Engineering', value: 10 },
-  { label: 'Data Engineering', value: 8 },
-  { label: 'Data Analysis', value: 3 },
-  { label: 'Data Science', value: 6 },
-]
