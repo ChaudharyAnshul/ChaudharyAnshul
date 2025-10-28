057ee7a261ebb507c7606ce37cebf4e034ad6015
ChaudharyAnshul <chaudhary.ans@northeastern.edu>
2025-10-27 23:56:20 -0400

    cards updated

--- src/sections/animated-folder-Card.jsx
+++ src/sections/animated-folder-Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Box from '@mui/material/Box';
@@ -8,13 +8,16 @@ import Grid from '@mui/material/Grid';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Link from '@mui/material/Link';
+import Dialog from '@mui/material/Dialog';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 const ImageBox = styled(Box)(({ theme }) => ({
   position: 'relative',
   display: 'flex',
   justifyContent: 'center',
   '&:hover img': {
-    transform: 'scale(1.3)',
+    transform: 'scale(1.1)',
   },
 }));
 
@@ -25,46 +28,85 @@ const Image = styled('img')(({ theme }) => ({
 }));
 
 export default function FolderCard({ title, subheader }) {
+  const [open, setOpen] = useState(false);
+  const [selectedUrl, setSelectedUrl] = useState('');
 
   const handleClick = (url) => {
-    alert('You Found the Hidden Link! \n You are being redirected!');
-    window.open(url, '_blank');
+    setSelectedUrl(url);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedUrl('');
   };
 
   return (
-    <Card sx={{ width:"100%"}}>
+    <Card sx={{ width: "100%" }}>
       <CardHeader title={title} subheader={subheader} />
       <CardContent>
         <Grid container spacing={2} justifyContent="center">
           <Grid item xs={4} sm={4} md={4} lg={4}>
-            <Link href="#" onClick={() => handleClick("https://se.chaudharyanshul.com/")}>
-            <div onClick={() => handleClick("https://se.chaudharyanshul.com/")}>
+            <Link href="#" onClick={(e) => { e.preventDefault(); handleClick("https://se.chaudharyanshul.com/"); }}>
               <ImageBox>
-                <Image src="assets/folders/web-dev-folder.png" alt="Image 1"/>
-              </ImageBox>
-            </div>
-            </Link>
-          </Grid>
-          
-          <Grid item xs={4} sm={4} md={4} lg={4}>
-            <Link  href="#" onClick={() => handleClick("https://de.chaudharyanshul.com/")}>
-              <ImageBox>
-                <Image src="assets/folders/data-pipeline-folder.png" alt="Image 2"/>
+                <Image src="assets/folders/web-dev-folder.png" alt="Software Engineering" />
               </ImageBox>
             </Link>
           </Grid>
+
           <Grid item xs={4} sm={4} md={4} lg={4}>
-            <Link  href="#" onClick={() => handleClick("https://ds.chaudharyanshul.com/")}>
+            <Link href="#" onClick={(e) => { e.preventDefault(); handleClick("https://de.chaudharyanshul.com/"); }}>
               <ImageBox>
-                <Image src="assets/folders/analysis-folder.png" alt="Image 3"/>
+                <Image src="assets/folders/data-pipeline-folder.png" alt="Data Engineering" />
+              </ImageBox>
+            </Link>
+          </Grid>
+
+          <Grid item xs={4} sm={4} md={4} lg={4}>
+            <Link href="#" onClick={(e) => { e.preventDefault(); handleClick("https://ds.chaudharyanshul.com/"); }}>
+              <ImageBox>
+                <Image src="assets/folders/analysis-folder.png" alt="Data Science" />
               </ImageBox>
             </Link>
           </Grid>
         </Grid>
       </CardContent>
+
+      {/* Popup Dialog */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        maxWidth="lg"
+        fullWidth
+        PaperProps={{
+          sx: {
+            borderRadius: 3,
+            overflow: 'hidden',
+            bgcolor: 'black',
+          },
+        }}
+      >
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', p: 1 }}>
+          <IconButton onClick={handleClose} sx={{ color: 'white' }}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        {selectedUrl && (
+          <Box
+            component="iframe"
+            src={selectedUrl}
+            sx={{
+              width: '100%',
+              height: '85vh',
+              border: 'none',
+              backgroundColor: 'white',
+            }}
+          />
+        )}
+      </Dialog>
     </Card>
   );
-};
+}
 
 FolderCard.propTypes = {
   subheader: PropTypes.string,
